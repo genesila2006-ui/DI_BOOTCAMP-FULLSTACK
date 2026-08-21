32d2f3cc93553a4db620151b15a0ab520859178c
eugeneasila20-wq <eugeneasila.20@gmail.com>
2026-08-21 12:32:32 +0300

    add Day 5 challenges

---
+++ Week 1/Day 5/Challenges #2.py
@@ -0,0 +1,14 @@
+n = 3
+for i in range(1, n + 1):
+    spaces = " " * (n - i)
+    stars = "*" * (2 * i - 1)
+    print(spaces + stars)
+#Pattern 2:
+
+#Python
+n = 5
+for i in range(1, n + 1):
+    spaces = " " * (n - i)
+    stars = "*" * i
+    print(spaces + stars)
+#Pattern 3:
